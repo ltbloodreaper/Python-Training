0896146881410ad4397e4d25aed2f6c186a0c4c3
ltbloodreaper <156793198+ltbloodreaper@users.noreply.github.com>
2024-01-19 12:47:16 +0000

    Delete Ej7-Print Filename extension.py

--- Ej7-Print Filename extension.py
+++
@@ -1,8 +0,0 @@
-# Write a Python program that accepts a filename from the user and prints the extension of the file.
-# Sample filename : abc.java
-# Output : java
-
-
-filename = input("Please insert the name of a file with extension: ")
-
-print(filename[filename.find(".")+1:])
