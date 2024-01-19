9a763cbd84155383b4d49f9ab300b6cbf5aee70e
ltbloodreaper <156793198+ltbloodreaper@users.noreply.github.com>
2024-01-19 12:46:52 +0000

    Delete Ej3-Display Current Time and date.py

--- Ej3-Display Current Time and date.py
+++
@@ -1,7 +0,0 @@
-#Write a Python program to display the current date and time
-
-import  datetime
-
-date = datetime.datetime.now()
-
-print("Current date and time is:\n",date.strftime("%Y-%m-%d %H:%M:%S"))
