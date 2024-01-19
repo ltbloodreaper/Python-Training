51a8971da240ab5c9deb344530128e143287b7e6
ltbloodreaper <156793198+ltbloodreaper@users.noreply.github.com>
2024-01-19 12:46:59 +0000

    Delete Ej4-Area of a circle with radius.py

--- Ej4-Area of a circle with radius.py
+++
@@ -1,9 +0,0 @@
-#Write a Python program that calculates the area of a circle based on the radius entered by the user.
-
-import math
-
-r=float(input("Please input a radius: "))
-
-print("{:.2f}".format(math.pi * (r**2)))
-
-print(math.pi * (r**2))
